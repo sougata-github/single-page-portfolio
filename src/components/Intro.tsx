2e9412a76f70a4378aa1c09b6f39b21b44b873f1
Sougata <sougatadas9874@gmail.com>
2025-01-25 17:50:56 +0530

    add intro section animation.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,40 @@
+"use client";
+
+import { stagger, useAnimate, useInView } from "framer-motion";
+import { useEffect } from "react";
+import SplitType from "split-type";
+
 const Intro = () => {
+  const [scope, animate] = useAnimate();
+
+  const inView = useInView(scope, {
+    once: true,
+  });
+
+  useEffect(() => {
+    new SplitType(scope.current.querySelector("h2"), {
+      types: "lines,words",
+      tagName: "span",
+    });
+  }, [scope]);
+
+  useEffect(() => {
+    if (inView) {
+      animate(
+        scope.current.querySelectorAll(".word"),
+        {
+          transform: "translateY(0%)",
+        },
+        {
+          duration: 0.5,
+          delay: stagger(0.2),
+        }
+      );
+    }
+  }, [animate, inView, scope]);
+
   return (
-    <section className="section mt-12 md:mt-16 lg:mt-20" id="intro">
+    <section className="section mt-12 md:mt-16 lg:mt-20" id="intro" ref={scope}>
       <div className="container max-md:!max-w-full">
         <h2 className="text-4xl md:text-7xl lg:w-[80%] lg:text-8xl">
           Building beautiful websites with clean code and thoughtful design to
