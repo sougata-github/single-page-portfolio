e3a5713848fc27a318653e54531e0fdfc2155622
Sougata <sougatadas9874@gmail.com>
2025-01-26 19:56:58 +0530

    add faq animation.

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,36 +1,60 @@
+"use client";
+
 import { faqs } from "@/constants";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
 const FAQs = () => {
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+  const handleExpand = (index: number) => {
+    const newExpanded: Record<string, boolean> = {};
+
+    faqs.forEach((item) => {
+      if (item.index !== index) {
+        newExpanded[item.index] = false;
+      }
+    });
+
+    setExpanded((prevExpanded) => ({
+      ...newExpanded,
+      [index]: !prevExpanded[index],
+    }));
+  };
+
   return (
     <section className="section" id="faqs">
       <div className="container !max-w-full">
         <h2 className="text-4xl md:text-7xl lg:text-8xl">FAQs</h2>
         <div className="mt-10 md:mt-16 lg:mt-20">
-          {faqs.map(({ question }) => (
+          {faqs.map(({ question, answer, index }) => (
             <div
               key={question}
-              className="border-t border-stone-400 border-dotted py-6 md:py-8 lg:py-10 last:border-b"
+              className="border-t border-stone-400 border-dotted py-6 md:py-8 lg:py-10 last:border-b cursor-pointer"
+              onClick={() => handleExpand(index)}
             >
               <div className="flex items-center justify-between gap-4">
                 <div className="text-2xl md:text-3xl lg:text-4xl">
                   {question}
                 </div>
                 <div className="inline-flex items-center justify-center size-11 border border-stone-400 rounded-full shrink-0">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 4.5v15m7.5-7.5h-15"
-                    />
-                  </svg>
+                  <Plus
+                    className="transition-all duration-200"
+                    style={{
+                      transform: `${
+                        expanded[index] ? "rotate(45deg)" : "rotate(0deg)"
+                      }`,
+                    }}
+                  />
                 </div>
+              </div>
+              <div
+                className="overflow-hidden transition-all duration-500"
+                style={{
+                  height: `${expanded[index] ? "100px" : "0px"}`,
+                }}
+              >
+                <p className="text-xl mt-4 max-w-xl">{answer}</p>
               </div>
             </div>
           ))}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -32,21 +32,25 @@ export const navItems = [
 
 export const faqs = [
   {
+    index: 0,
     question: "How long does it take to build a website?",
     answer:
       "It depends on the complexity of the website and the scope of the project.",
   },
   {
+    index: 1,
     question: "What is your development process like?",
     answer:
       "I follow a hands-on approach starting with project planning, building out the core features, and regular check-ins to make sure everything matches your needs.",
   },
   {
+    index: 2,
     question: "Do you work with international clients?",
     answer:
       "Yes, I work with clients globally and can accommodate different time zones for meetings and communication.",
   },
   {
+    index: 3,
     question: "What industries do you specialize in?",
     answer:
       "I have experience across various industries including technology, retail, hospitality, and professional services, bringing fresh perspectives to each project.",
